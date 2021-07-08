95903fc14df22b321dc1caae8a090499dc81479f
elkorospace <bagaselkoro@gmail.com>
2021-07-08 09:08:01 +0700

    refactoring 9: change base url

--- app/config/Config.php
+++ app/config/Config.php
@@ -1,6 +1,6 @@
 <?php
 
-define('BASE_URL','https://t-gadget.herokuapp.com/');
+define('BASE_URL','https://tgadgetid.herokuapp.com/');
 define('API_URL','https://koroweb-test.000webhostapp.com/Api/');
 
 // define('BASE_URL','http://localhost/tgadget/');
