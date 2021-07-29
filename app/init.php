7355dfb23c5fbe8adafbf838d6ab56318a2286af
elkorospace <bagaselkoro@gmail.com>
2021-07-29 09:42:16 +0700

    change base url

--- app/init.php
+++ app/init.php
@@ -4,8 +4,8 @@ spl_autoload_register(function($file){
     require_once 'core/'.$file.'.php';
 });
 
-define('BASE_URL','http://localhost/t-gadget/');
-// define('BASE_URL','https://t-gadget.herokuapp.com/');
+// define('BASE_URL','http://localhost/t-gadget/');
+define('BASE_URL','https://t-gadget.herokuapp.com/');
 
 // define('API_URL' ,'http://localhost/t-gadgetapi/');
 define('API_URL' ,'https://t-gadgetapi.herokuapp.com/');
